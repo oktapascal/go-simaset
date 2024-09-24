1828a6e078b3781c893867b0f7983a1bc2db8acc
oktapascal <oktaiscool@gmail.com>
2024-09-24 08:59:31 +0700

    create menu handler

--- app/menu/handler.go
+++ app/menu/handler.go
@@ -1,8 +1,11 @@
 package menu
 
 import (
+	"encoding/json"
+	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
 	"github.com/oktapascal/go-simpro/model"
+	"github.com/oktapascal/go-simpro/web"
 	"net/http"
 )
 
@@ -12,11 +15,47 @@ type Handler struct {
 }
 
 func (hdl *Handler) GetMenus() http.HandlerFunc {
-	//TODO implement me
-	panic("implement me")
+	return func(writer http.ResponseWriter, request *http.Request) {
+		ctx := request.Context()
+		result := hdl.svc.GetMenus(ctx)
+
+		svcResponse := web.DefaultResponse{
+			Code:   http.StatusOK,
+			Status: http.StatusText(http.StatusOK),
+			Data:   result,
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
+
+		encoder := json.NewEncoder(writer)
+
+		err := encoder.Encode(svcResponse)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func (hdl *Handler) GetMenuChildren() http.HandlerFunc {
-	//TODO implement me
-	panic("implement me")
+	return func(writer http.ResponseWriter, request *http.Request) {
+		menuId := chi.URLParam(request, "menuId")
+
+		ctx := request.Context()
+		result := hdl.svc.GetMenuChildren(ctx, menuId)
+
+		svcResponse := web.DefaultResponse{
+			Code:   http.StatusOK,
+			Status: http.StatusText(http.StatusOK),
+			Data:   result,
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
+
+		encoder := json.NewEncoder(writer)
+
+		err := encoder.Encode(svcResponse)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
