a496f3f53496e6c6f7d20e1bef5680d8ec0f2a1a
oktapascal <oktaiscool@gmail.com>
2024-09-24 08:50:42 +0700

    create menu repository

--- app/menu/repository.go
+++ app/menu/repository.go
@@ -9,11 +9,59 @@ import (
 type Repository struct{}
 
 func (rpo *Repository) GetMenus(ctx context.Context, tx *sql.Tx) *[]model.Menu {
-	//TODO implement me
-	panic("implement me")
+	query := "select id, name, icon_component, path_url from menus where deleted_at is null order by indeks"
+
+	rows, err := tx.QueryContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		err := rows.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	var menus []model.Menu
+	for rows.Next() {
+		var menu model.Menu
+		err = rows.Scan(&menu.Id, &menu.Name, &menu.IconComponent, &menu.PathUrl)
+		if err != nil {
+			panic(err)
+		}
+
+		menus = append(menus, menu)
+	}
+
+	return &menus
 }
 
 func (rpo *Repository) GetMenuChildren(ctx context.Context, tx *sql.Tx, menuId string) *[]model.MenuChild {
-	//TODO implement me
-	panic("implement me")
+	query := "select id, name, path_url from menu_childs where menu_id = ? and deleted_at is null order by indeks"
+
+	rows, err := tx.QueryContext(ctx, query, menuId)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		err := rows.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	var menus []model.MenuChild
+	for rows.Next() {
+		var menu model.MenuChild
+		err = rows.Scan(&menu.Id, &menu.Name, &menu.PathUrl)
+		if err != nil {
+			panic(err)
+		}
+
+		menus = append(menus, menu)
+	}
+
+	return &menus
 }
