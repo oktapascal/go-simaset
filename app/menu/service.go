502a187281ee1fc99b873f10f4cab5366277b20d
oktapascal <oktaiscool@gmail.com>
2024-09-24 08:55:32 +0700

    create menu service

--- app/menu/service.go
+++ app/menu/service.go
@@ -3,6 +3,7 @@ package menu
 import (
 	"context"
 	"database/sql"
+	"github.com/oktapascal/go-simpro/helper"
 	"github.com/oktapascal/go-simpro/model"
 )
 
@@ -12,11 +13,54 @@ type Service struct {
 }
 
 func (svc *Service) GetMenus(ctx context.Context) []model.MenuResponse {
-	//TODO implement me
-	panic("implement me")
+	tx, err := svc.db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	defer helper.CommitRollback(tx)
+
+	menus := svc.rpo.GetMenus(ctx, tx)
+
+	var result []model.MenuResponse
+	if len(*menus) > 0 {
+		for _, value := range *menus {
+			menu := model.MenuResponse{
+				Id:            value.Id,
+				Name:          value.Name,
+				IconComponent: value.IconComponent,
+				PathUrl:       value.PathUrl,
+			}
+
+			result = append(result, menu)
+		}
+	}
+
+	return result
 }
 
 func (svc *Service) GetMenuChildren(ctx context.Context, menuId string) []model.MenuChildResponse {
-	//TODO implement me
-	panic("implement me")
+	tx, err := svc.db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	defer helper.CommitRollback(tx)
+
+	menus := svc.rpo.GetMenuChildren(ctx, tx, menuId)
+
+	var result []model.MenuChildResponse
+	if len(*menus) > 0 {
+		for _, value := range *menus {
+			menu := model.MenuChildResponse{
+				Id:      value.Id,
+				Name:    value.Name,
+				PathUrl: value.PathUrl,
+			}
+
+			result = append(result, menu)
+		}
+	}
+
+	return result
 }
